b9b6cda59958901f2efd81dfdb3c7c2761c80f0a
mdzobayer <mdzobayer@rocketmail.com>
2019-01-29 09:24:53 +0600

    Added CF  978C

--- Codeforces/978 C. Letters.cpp
+++ Codeforces/978 C. Letters.cpp
@@ -11,48 +11,59 @@ ll sums[200000+5], x;
 void solve() {
     ll low = 0, high = n - 1, mid;
 
-    while(low < high) {
+    while(low <= high) {
         mid = (low + high) / 2;
 
         if(sums[mid] > x) {
             high = mid - 1;
         }
         else if(sums[mid] < x) {
-            low = mid;
+            low = mid + 1;
         }
         else {
             low = mid;
             break;
         }
     }
-    cout << "low = " << low << " " << endl;
-    if(low == 0 && x <= sums[0]) {
-        cout << low + 1 << " " << x << endl;
+    //cout << "low = " << low << " " << endl;
+    if(low == 0) {
+        if(sums[low] >= x )
+            cout << 1 << " " << x << endl;
+        else {
+            cout << 2 << " " << (sums[low + 1] - x) - sums[low] << endl;
+        }
     }
-    else if (x >= sums[low]) {
-        ++low;
-        cout << low + 1 << " " << (sums[low] - x) - x << endl;
+    else if (low > 0) {
+        //printf("%lld %lld\n", sums[low - 1], x);
+        if(sums[low] >= x ) {
+            //cout << "Debug 1" << endl;
+
+            cout << low + 1 << " " << x - sums[low - 1] << endl;
+        }
+        else {
+            cout << low << " " << (x - sums[low - 1]) << endl;
+        }
     }
 }
 
 int main() {
 
-    freopen("in.txt", "r", stdin);
+    //freopen("in.txt", "r", stdin);
 
     ll i;
-    scanf("%lld %lld", &n, &m);
+    cin >> n >> m;
 
-    scanf("%lld", &arr[0]);
+    cin >> arr[0];
     sums[0] = arr[0];
 
     for (i = 1; i < n; ++i) {
-        scanf("%lld", &arr[i]);
-        sums[i] = arr[i] + arr[i - 1];
+        cin >> arr[i];
+        sums[i] = arr[i] + sums[i - 1];
     }
 
     for (i = 0; i < m; ++i) {
-        scanf("%lld", &x);
-        cout << x << endl;
+        cin >> x;
+        //cout << x << endl;
         solve();
     }
 
