d77d00070380ab9bec75d8124cab0427f24f8155
mdzobayer <mdzobayer@rocketmail.com>
2019-04-17 19:51:58 +0600

    Adding Emoji in C++

---
+++ C++ Programming/Glut/Drawing Emoji/Smile(Green).cpp
@@ -0,0 +1,39 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main()
+{
+
+	int graphicdriver = DETECT, graphicmode;
+
+	// calling initgraph function with 
+	initgraph(&graphicdriver,&graphicmode,"c:\\turboc3\\bgi");
+
+	// Printing message for user
+	outtextxy(10, 10 + 10, "A smiley face in C++ graphics");
+	
+	
+	// Drawing Face with GREEN color 
+	setcolor(GREEN);					// Setting Color GREEN
+	circle(300, 100, 40);				
+	setfillstyle(SOLID_FILL, GREEN);	// Making Solid color, otherwise white
+	floodfill(300, 100, GREEN);
+
+	//setting color to black
+	setcolor(BLACK);
+	setfillstyle(SOLID_FILL, BLACK);
+
+	// Drawing Eyes
+	fillellipse(310, 85, 2, 6);		// Right Eyes
+	fillellipse(290, 85, 2, 6);		// Left Eyes
+
+	// Drawing Mouth
+	ellipse(300, 100, 205, 335, 20, 9);
+	ellipse(300, 100, 205, 335, 20, 10);
+	ellipse(300, 100, 205, 335, 20, 11);
+
+
+	getch();
+	return 0;	
+}
+
