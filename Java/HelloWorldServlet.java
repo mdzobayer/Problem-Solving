c423dc556bb4f7afdee3a3a5052da40fe45bd187
mdzobayer <mdzobayer@rocketmail.com>
2019-05-26 08:51:00 +0600

    Add Servlet first examples

---
+++ Java/HelloWorldServlet.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.servlet.*;
+
+public class HelloWorldServlet extends GenericServlet {
+    public void service (ServletRequest request, ServletResponse response)
+                throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("Hello, world!");
+        pw.close();
+    }
+}
