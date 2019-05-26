43b651834f39a0f0a70b9f3831b9dfcbc54ad2dd
mdzobayer <mdzobayer@rocketmail.com>
2019-05-26 08:52:29 +0600

    Add Java Swing first examples

---
+++ Java/SwingExample.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class SwingExample {
+    SwingExample () {
+        JFrame frame = new JFrame("Hello, World! Application");
+        frame.setSize(350,150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Hello, World!");
+        frame.add(label);
+        frame.setVisible(true);
+    }
+    public static void main(String [] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run () {
+                new SwingExample();
+            }
+        } );
+    }
+}
