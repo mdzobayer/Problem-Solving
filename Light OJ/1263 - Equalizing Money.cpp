7258195dff2085c4e34fd7c2a05d8aab5f4081ef
mdzobayer <mdzobayer@rocketmail.com>
2019-05-29 09:41:07 +0600

    1263 - Equalizing Money solutions

--- Light OJ/1263 - Equalizing Money.cpp
+++ Light OJ/1263 - Equalizing Money.cpp
@@ -4,97 +4,87 @@ using namespace std;
 
 vector < int > ajlist[SIZE];
 bool visit[SIZE];
-int n, m, arr[SIZE], x;
+int n, m, arr[SIZE], sum, equalMoney;
 
-bool bfs(int extra, int cNode) {
+bool bfs(int cNode) {
 
-    memset(visit, true, sizeof(visit));
-    int nNode, i;
-
-    visit[cNode] = false;
-    queue < int > q;
+    int nNode, i, sz, amount = 0, person = 0;
+    queue <int> q;
     q.push(cNode);
-
+    visit[cNode] = false;
+    // printf("Current Nodes: ");
     while(!q.empty()) {
         cNode = q.front();
         q.pop();
-        for (i = 0; i < ajlist[cNode].size(); ++i) {
+        // printf(" %d", cNode);
+        ++person;
+        amount += arr[cNode];
+
+        sz = ajlist[cNode].size();
+        for (i = 0; i < sz; ++i) {
             nNode = ajlist[cNode][i];
-            if(visit[nNode]) {
+            if (visit[nNode]) {
                 visit[nNode] = false;
-                if(arr[nNode] < x) {
-                    arr[nNode] += extra;
-                    extra = arr[nNode] - x;
-                    if(extra <= 0) return true;
-                }
                 q.push(nNode);
             }
         }
     }
-    if(extra > 0) return false;
+    // puts("");
+    // printf("check %d %d\n", equalMoney * person, amount);
 
-    return true;
+    if (equalMoney * person == amount)
+        return true;
+    else return false;
 }
 
 int main() {
 
-    freopen("in.txt", "r" ,stdin);
+    //freopen("in.txt", "r", stdin);
 
     int test, t, i, u, v;
-    long long int sum;
-    bool res;
-
+    bool ans;
     scanf("%d", &test);
+
     for (t = 1; t <= test; ++t) {
         scanf("%d %d", &n, &m);
-
-        // Clear ajlist
-
-
         sum = 0;
+        
         for (i = 1; i <= n; ++i) {
-            ajlist[i].clear();
             scanf("%d", &arr[i]);
             sum += arr[i];
+            ajlist[i].clear();
         }
-        ajlist[n].clear();
-        ajlist[n + 1].clear();
+        ajlist[i].clear();
+        // printf("Sum is %d\n", sum);
 
         for (i = 0; i < m; ++i) {
             scanf("%d %d", &u, &v);
             ajlist[u].push_back(v);
             ajlist[v].push_back(u);
         }
-        x = sum / n;
-        if(x * n == sum) {
-            //printf("Case %d: %s\n", t, (bfs() ? "Yes" : "No"));
-            res = true;
-            for (i = 1; i <= n && res; ++i) {
-                //cout << "Grader check = " << arr[i] << " " << x << endl;
-                if(arr[i] > x) {
-                    cout << "Node i = " << i << " is called" << endl;
-                    res = bfs(arr[i] - x, i);
-                    arr[i] = x;
+        printf("Case %d: ", t);
+        if (sum % n) {
+            puts("No");
+        }
+        else {
+            memset(visit, true, sizeof(visit));
+            equalMoney = sum / n;
+            ans = true;
+            for (i = 1; i <= n; ++i) {
+                if (visit[i]) {
+                    ans = bfs(i);
+                    
                 }
-            }
-            cout << "Debug 2 res = " << res << endl;
-            for (i = 1; i <= n && res; ++i) {
-                if(arr[i] != x) {
-                    //cout << x << " " << arr[i] << endl;
-                    //cout << "Debug 3 res = " << res << " i = " << i << endl;
-                    res = false;
+                if (ans == false) {
+                    puts("No");
                     break;
                 }
             }
-            //cout << "Debug 1 here" << endl;
-            if(res) printf("Case %d: Yes\n", t);
-            else printf("Case %d: No\n", t);
+            if (ans) {
+                puts("Yes");
+            } 
         }
-        else {
-            printf("Case %d: No\n", t);
-        }
-
     }
-
+    
     return (0);
 }
