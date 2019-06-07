04a7ebec1a0e461bf80eb55aa9844af6dc910b67
mdzobayer <mdzobayer@rocketmail.com>
2019-06-07 18:59:46 +0600

    Add 1125 - Divisible Group Sums solution

--- Light OJ/1125 - Divisible Group Sums.cpp
+++ Light OJ/1125 - Divisible Group Sums.cpp
@@ -4,22 +4,54 @@ using namespace std;
 
 int n, q, d, m, arr[200+5];
 
-//ll dp[203][23][12];
+ll dp[203][46][12];
 
 ll solve (int pos, int rem, int cnt) {
-    if (cnt == m && rem == 0) return 1;
+    if (cnt == 0 && rem == 0) {
+        return 1;
+    }
+    //printf("pos %d rem %d cnt %d\n", pos, rem, cnt);
+    if (pos < 0 || cnt < 0) return 0;
 
-    if (pos >= n) return 0;
+    
+
+    int nRem = -100;
+    if (rem < 0) {
+        nRem = (-1) * rem;
+        nRem += 23;
+    }
+
+   // printf("pos %d rem %d cnt %d\n", pos, rem, cnt);
+    
+    if (nRem == -100) {
+        if (dp[pos][rem][cnt] != -1) {
+            return dp[pos][rem][cnt];
+        }
+    }
+    else {
+        if (dp[pos][nRem][cnt] != -1) {
+            return dp[pos][nRem][cnt];
+        }
+    }
+    //printf("pos %d rem %d nRem %d cnt %d\n", pos, rem, nRem, cnt);
 
     ll ans = 0;
-    ans += solve(pos + 1, rem, cnt);    // Skip Current element
-    ans += solve(pos + 1, (rem + arr[pos]) % d, cnt + 1);   // Add current element
+    ans += solve(pos - 1, rem, cnt);    // Skip Current element
+    ans += solve(pos - 1, (rem + arr[pos]) % d, cnt - 1);   // Add current element
+
+    if (nRem == -100)
+        dp[pos][rem][cnt] = ans;
+    else 
+        dp[pos][nRem][cnt] = ans;
 
     return ans;
 }
 
 
 int main() {
+
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
 
     int test, t, i;
     scanf("%d", &test);
@@ -33,9 +65,15 @@ int main() {
         for (i = 0; i < q; ++i) {
             scanf("%d %d", &d, &m);
 
-            //memset(dp, -1, sizeof(dp));
+            if (m <= n) {
 
-            printf("%lld\n", solve(0, 0, 0));
+                memset(dp, -1, sizeof(dp));
+
+                printf("%lld\n", solve(n - 1, 0, m));
+            }
+            else {
+                puts("0");
+            }
         }
     }
 
