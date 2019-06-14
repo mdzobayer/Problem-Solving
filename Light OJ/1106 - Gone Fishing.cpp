9962daeb9b86b071eb8cc6b9501c025a7289225e
mdzobayer <mdzobayer@rocketmail.com>
2019-06-14 12:42:12 +0600

    1106 - Gone fishing with 2 arguments path print remain

--- Light OJ/1106 - Gone Fishing.cpp
+++ Light OJ/1106 - Gone Fishing.cpp
@@ -3,32 +3,57 @@
 #define TIME 200
 using namespace std;
 
-int n, h, xTime, maxTime, fishes[N], decreRate[N], movingCost[N], dp[N][TIME][TIME];
+int n, h, xTime, maxTime, fishes[N], decreRate[N], movingCost[N], dp[N][TIME];
 bool flag;
 
 int solve(int lakeNo, int remainTime) {
     
     if (remainTime <= 0 || lakeNo > n) return (0);
     //printf("Lake no : %d RemainTime : %d\n", lakeNo, remainTime);
+
+    if (dp[lakeNo][remainTime] != -1) {
+        return dp[lakeNo][remainTime];
+    }
+
     int spendTime, availableFish, found = 0, ans = 0;
 
-    for (spendTime = 0; (remainTime - spendTime) > 0; ++spendTime) {
+    for (spendTime = 0; spendTime < remainTime; ++spendTime) {
 
         availableFish = fishes[lakeNo] - (decreRate[lakeNo] * spendTime);
-        xTime = remainTime - (spendTime + movingCost[lakeNo]);
+        xTime = remainTime - (1 + spendTime + movingCost[lakeNo]);
 
         if (availableFish < 0) availableFish = 0;
 
-        if (xTime > 0) {
+        //if (xTime > 0) {
             ans = max(ans, solve(lakeNo + 1, xTime) + availableFish + found);
-        }
-        else {
-            ans = max(ans, availableFish + found);
-        }
+        //}
+        //else {
+            //ans = max(ans, availableFish + found);
+        //}
         found += availableFish;
     }
 
-    return ans;
+    return dp[lakeNo][remainTime] = ans;
+}
+
+void PrintPath(int lakeNo, int remainTime) {
+    //printf("Lake no %d time %d\n", lakeNo, remainTime);
+    if (lakeNo > n) return;
+    int tm = 0, t, ans = 0;
+    for (t = 0; t < remainTime; ++t) {
+        if (dp[lakeNo][t] >= ans) {
+            ans = dp[lakeNo][t];
+            tm = t;
+        }
+    }
+    if (lakeNo == 1) {
+        printf("%d", (tm));
+    }
+    else {
+        printf(", %d", (tm));
+    }
+    //printf(", %d", tm * 5);
+    PrintPath(lakeNo + 1, remainTime - (tm + 1));
 }
 
 
@@ -60,7 +85,7 @@ int main() {
         ans = solve(1, maxTime);
 
         printf("Case %d:\n", t);
-        //PrintPath(1, 0, 0);
+        PrintPath(1, maxTime);
         printf("\nNumber of fish expected: %d\n", ans);
     }
 
