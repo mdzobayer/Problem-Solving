895d76c1d59efb35392c710c8a0769039070a004
mdzobayer <mdzobayer@rocketmail.com>
2021-01-15 10:41:41 +0600

    uva 991 safe salutations solution added

---
+++ Uva/991 - Safe Salutations.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll dp[12];
+
+ll catalan(ll n) {
+    if (n < 1)
+    return 1;
+
+    if (dp[n] != -1)
+        return dp[n];
+    
+    dp[n] = 0;
+    for (ll i = 0; i < n; ++i) {
+        dp[n] += (catalan(i) * catalan(n-i-1));
+    }
+
+    return dp[n];
+}
+
+
+int main() {
+
+    memset(dp, -1, sizeof(dp));
+    for (ll i = 0; i < 11; ++i) {
+        catalan(i);
+    }
+
+    ll n;
+    bool isFirst = true;
+    
+    while (scanf("%lld", &n) == 1)
+    {
+        if (!isFirst) {
+            puts("");
+        }
+        printf("%lld\n", dp[n]);
+        isFirst = false;
+    }
+    
+    return (0);
+}
