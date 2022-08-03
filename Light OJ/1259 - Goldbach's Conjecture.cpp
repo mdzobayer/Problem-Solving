23fffa721832d97be9d8eb2ab14bfe17ab2b9ef1
mdzobayer <muhammad.zobayer@selise.ch>
2022-08-04 05:45:51 +0600

    Lightoj: goldbach's conjecture code snippet added

---
+++ Light OJ/1259 - Goldbach's Conjecture.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int test, t, n;
+    scanf("%d", &test);
+    for (t = 0; t < test; ++t) {
+        scanf("%d", &n);
+
+        printf("Case %d: 1\n", t + 1);
+    }
+
+
+    return (0);
+}
