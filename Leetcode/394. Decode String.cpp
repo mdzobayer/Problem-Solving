16a8a2f5e43f3f13db7fea8ccf5514a11c207e08
mdzobayer <mdzobayer@rocketmail.com>
2023-09-26 06:06:44 +0600

    leetcode: 394. Decode String solution added

--- Leetcode/394. Decode String.cpp
+++ Leetcode/394. Decode String.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string decodeString(string s) {
         int i, j, numberOfRepetition;
         string ans = "", decodedStr = "", repeatedStr, numberOfRepetitionStr = "";
-        bool isDigitStarted = true;
+        bool isDigitStarted = true, isSingleStr = false;
         
         pair<int, string> tempPair;
         stack<pair<int, string>> stk;
@@ -32,8 +32,23 @@ string decodeString(string s) {
                 
             }
             else if (isalpha(s[i])) {
-                decodedStr += s[i];
-                isDigitStarted = false;
+                if(isDigitStarted) {
+                    if (!stk.empty()) {
+                        tempPair = stk.top();
+                        stk.pop();
+
+                        tempPair.second += s[i];
+                        stk.push(tempPair);
+                    }
+                    else {
+                       ans += s[i]; 
+                    }
+                    
+                }
+                else {
+                    decodedStr += s[i];
+                    isDigitStarted = false;
+                }
             }
             else if (s[i] == ']') {
 
@@ -51,7 +66,7 @@ string decodeString(string s) {
                 // Process task
                 tempPair = stk.top();
                 stk.pop();
-                cerr << "tempPair " << tempPair.first << " " << tempPair.second << endl;
+                //cerr << "tempPair " << tempPair.first << " " << tempPair.second << endl;
 
                 repeatedStr = "";
                 for (j = 0; j < tempPair.first; ++j) {
@@ -61,6 +76,9 @@ string decodeString(string s) {
                 if (stk.empty()) {
                     ans += repeatedStr;
                 } else {
+                    tempPair = stk.top();
+                    stk.pop();
+
                     tempPair.second += repeatedStr;
                     stk.push(tempPair);
                 }
@@ -78,8 +96,11 @@ int main() {
     string s1 = "3[a]2[bc]";
     string s2 = "3[2[c]]2[b]";
     string s3 = "3[a2[c]]2[bc]";
+    string s4 = "2[abc]3[cd]ef";
+    string s5 = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
+    string s6 = "xyz3[2[c]uk3[usa]]ef";
 
-    cout << decodeString(s3) << endl;
+    cout << decodeString(s5) << endl;
 
     return (0);
 }
