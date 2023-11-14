98b235fd86dd455cbbaf37e5e01d551d5fcaccc6
mdzobayer <mdzobayer@rocketmail.com>
2023-11-14 21:51:05 +0600

    leetcode: 48. Rotate Image solution added

---
+++ Leetcode/48. Rotate Image.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int i, j, temp, n = matrix.size();
+
+        for (i = 0; i < n / 2; ++i) {
+            for (j = i; j < n - i - 1; ++j) {
+                temp = matrix[i][j];
+
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
+};
