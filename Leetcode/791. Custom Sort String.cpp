0625885868fb8a994abe0d23bee246e2a28b51f2
mdzobayer <mdzobayer@rocketmail.com>
2024-03-11 16:39:36 +0600

    leetcode: 791. Custom Sort String fastest solution added

--- Leetcode/791. Custom Sort String.cpp
+++ Leetcode/791. Custom Sort String.cpp
@@ -3,20 +3,23 @@ public:
     string customSortString(string order, string s) {
         int i, j, n = s.size();
         string ans = "";
-        vector<bool> taken(n, false);
+        vector<int> taken(26, 0);
+
+        for (i = 0; i < n; ++i) {
+            ++ taken[s[i] - 'a'];
+        }
 
         for (i = 0; i < order.size(); ++i) {
-            for (j = 0; j < n; ++j) {
-                if (s[j] == order[i]) {
-                    ans += s[j];
-                    taken[j] = true;
-                }
+            while(taken[order[i] - 'a'] > 0) {
+                ans += order[i];
+                -- taken[order[i] - 'a'];
             }
         }
 
-        for (i = 0; i < n; ++i) {
-            if (!taken[i]) {
-                ans += s[i];
+        for (i = 0; i < 26; ++i) {
+            while(taken[i] > 0) {
+                ans += (char)('a' + i);
+                -- taken[i];
             }
         }
 
