d6c9952fda421adc145c483b32f850a59eb3ffda
Zobayer Mahmud <mdzobayer@rocketmail.com>
2024-07-30 18:28:13 +0600

    leetcode: 1653. Minimum Deletions to Make String Balanced solution (2 pass) added

--- Leetcode/1653. Minimum Deletions to Make String Balanced.cpp
+++ Leetcode/1653. Minimum Deletions to Make String Balanced.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int i, n = s.size(), ans = n;
+        int i, j, n = s.size(), ans = n;
 
         vector<int> beforeB(n + 1, 0), afterA(n + 1, 0);
         
-        for (i = 0; i < n; ++i) {
+        for (i = 0, j = n - 1; i < n; ++i, --j) {
             if (s[i] == 'b') beforeB[i] = 1;
             if (i > 0) beforeB[i] += beforeB[i - 1];
-        }
 
-        for (i = n - 1; i >= 0; --i) {
-            if (s[i] == 'a') afterA[i] = 1;
-            if (i < n - 1) afterA[i] += afterA[i + 1];
+            if (s[j] == 'a') afterA[j] = 1;
+            if (j < n - 1) afterA[j] += afterA[j + 1];
         }
 
         for (i = 0; i < n; ++i) {
