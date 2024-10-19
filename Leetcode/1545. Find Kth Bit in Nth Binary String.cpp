d7503cb6727824ff704d2b82bcb1556ccb79b81f
mdzobayer <mdzobayer@rocketmail.com>
2024-10-19 15:56:55 +0600

    leetcode: 1545. Find Kth Bit in Nth Binary String solution added

---
+++ Leetcode/1545. Find Kth Bit in Nth Binary String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if (n == 1) return '0';
+
+        int len = 1 << n;
+
+        if (k < len / 2) {
+            return findKthBit(n - 1, k);
+        }
+        else if (k == len / 2) return '1';
+
+        char relatedBit = findKthBit(n - 1, len - k);
+        return (relatedBit == '0') ? '1' : '0';
+    }
+};
