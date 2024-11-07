c02e8ebdaba1743847fc9bee36cb56e10e71ed4f
mdzobayer <mdzobayer@rocketmail.com>
2024-11-07 19:44:11 +0600

    leetcode: 2275. Largest Combination With Bitwise AND Greater Than Zero solution added

---
+++ Leetcode/2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        
+        vector<int> bitCount(24, 0);
+        int i;
+
+        for (i = 0; i < 24; ++i) {
+            for (int num : candidates) {
+                if ((num & (1 << i))!= 0) {
+                    ++ bitCount[i];
+                }
+            }
+        }
+
+        return *max_element(bitCount.begin(), bitCount.end());
+    }
+};
