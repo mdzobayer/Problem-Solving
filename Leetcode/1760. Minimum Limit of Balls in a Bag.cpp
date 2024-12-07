c6549510c567be74f3cdb4cb7ed42aeaa66dd537
mdzobayer <mdzobayer@rocketmail.com>
2024-12-07 18:36:35 +0300

    leetcode: 1760. Minimum Limit of Balls in a Bag solution added

---
+++ Leetcode/1760. Minimum Limit of Balls in a Bag.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int left = 1, right = 0;
+
+        for (int num : nums) {
+            right = max(right, num);
+        }
+
+        while(left < right) {
+            int mid = (left + right) / 2;
+
+            if (isPossible(mid, nums, maxOperations)) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    bool isPossible(int maxBallsInBag, vector<int> & nums, int maxOperations) {
+        int totalOperations = 0;
+
+        for (int num : nums) {
+            int operations = ceil(num / (double)maxBallsInBag) - 1;
+            totalOperations += operations;
+
+            if (totalOperations > maxOperations) return false;
+        }
+
+        return true;
+    }
+};
