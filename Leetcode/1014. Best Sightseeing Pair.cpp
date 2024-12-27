d6de66dd7a8b820e990ce7de38511a983b1b6390
mdzobayer <mdzobayer@rocketmail.com>
2024-12-27 11:33:00 +0300

    leetcode: 1014. Best Sightseeing Pair solution

---
+++ Leetcode/1014. Best Sightseeing Pair.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int i, n = values.size(), maxScore = 0;
+        vector<int> maxLeftScore(n);
+        maxLeftScore[0] = values[0];
+
+        for (i = 1; i < n; ++i) {
+            int currentRightScore = values[i] - i;
+            maxScore = max(maxScore, maxLeftScore[i - 1] + currentRightScore);
+            int currentLeftScore = values[i] + i;
+            maxLeftScore[i] = max(maxLeftScore[i - 1], currentLeftScore);
+        }
+
+        return maxScore;
+    }
+};
